59735120f841e60e2f772c068eb2e20f81d5f310
pratiknu10 <pratikv@nu10.co>
2025-09-06 01:37:52 +0530

    ASD

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -108,7 +108,7 @@ export const loginUser = async (req, res) => {
     // Set cookie options for localhost
     const cookieOptions = {
       httpOnly: true,
-      secure: true, // false for localhost
+      secure: false, // false for localhost
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     };
