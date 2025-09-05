f2d13b6c9ee1de4a34971d26d0d9c7afbf3a3d02
pratiknu10 <pratikv@nu10.co>
2025-09-06 00:16:59 +0530

    ASDASD

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.use(
   cors({
     origin: process.env.ALLOWED_ORIGINS?.split(",") || [
       "http://localhost:8080",
+      "https://olx-frontend-nu.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
